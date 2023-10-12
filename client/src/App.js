2402774624321e78e620a7c18243f6b6eedb1fd3
jiohjung98 <ckaquz98@naver.com>
2023-10-12 15:48:46 +0900

    Remove: .git files

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SummaryPage from './pages/SummaryPage';
+import OrderPage from './pages/OrderPage';
+
+function App() {
+  return (
+    <div style={{ padding: '4rem' }}>
+      <OrderPage />
+      <SummaryPage />
+    </div>
+  );
+}
+
+export default App;
